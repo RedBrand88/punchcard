b0da007e358af0754b9a32776517c93351f02470
Brandon Bashein <bbashein@Brandons-MacBook-Pro.local>
2020-02-20 15:12:54 -0700

    refactored timecard class with staticmethod decorator

--- timecard.py
+++ timecard.py
@@ -3,36 +3,40 @@ from datetime import datetime
 
 class timecard:
     #timecard{"punch in":tuple, "lunch out":tuple, "lunch in":tuple, "punch out":tuple}
+
     def __init__(self):
         self.timein = self.getCurrentTime()
-        self.day = {"punch in":0,"lunch out":0,"lunch in":0,"punch out":0}
+        self.day = {"punch in":0,"lunch out":0, "lunch in":0, "punch out":0}
 
-    def getCurrentTime(self):
+    @staticmethod
+    def getCurrentTime():
         localtime = time.localtime(time.time())
         return localtime
 
-    def getToday(self):
+    @staticmethod
+    def getToday():
         today = datetime.now().strftime("%m-%d-%Y")
         return today
 
-    def formatTime(self, timestamp, label):
+    @staticmethod
+    def formatTime(timestamp, label):
         timeformated = time.asctime(timestamp)
         line = label + " : " + timeformated + "\n"
         return line
 
     #possibly change this method unless time tuple is cached
     def getPunchTime(self, key):
-        return self.formatTime(self.day[key], key)
+        return timecard.formatTime(self.day[key], key)
 
     def punch(self):
         if self.day["punch in"] == 0:
-            self.day["punch in"] = time.localtime(time.time())
+            self.day["punch in"] = timecard.getCurrentTime()
         elif self.day["lunch out"] == 0:
-            self.day["lunch out"] = time.localtime(time.time())
+            self.day["lunch out"] = timecard.getCurrentTime()
         elif self.day["lunch in"] == 0:
-            self.day["lunch in"] = time.localtime(time.time())
+            self.day["lunch in"] = timecard.getCurrentTime()
         elif self.day["punch out"] == 0:
-            self.day["punch out"] = time.localtime(time.time())
+            self.day["punch out"] = timecard.getCurrentTime()
 
     def setpunches(self, key, punchvalue):
         self.day[key] = punchvalue
